f6ed5c5c750df92796959fd60b7b94f25af2c545
Mr.Yang <815150141@qq.com>
2017-08-20 03:10:24 -0400

    AOP 流程切面

---
+++ src/main/java/com/xgj/aop/spring/advisor/ControlFlowAdvisor/ControlFlowAdvisorTest.java
@@ -0,0 +1,24 @@
+package com.xgj.aop.spring.advisor.ControlFlowAdvisor;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ControlFlowAdvisorTest {
+
+	@Test
+	public void test() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"classpath:com/xgj/aop/spring/advisor/ControlFlowAdvisor/conf-controlFlowAdvisor.xml");
+
+		Waiter waiter = ctx.getBean("waiter", Waiter.class);
+		waiter.greetTo("XiaoGongJiang");
+		waiter.serverTo("XiaoGongJiang");
+
+		WaiterDelegate waiterDelegate = new WaiterDelegate();
+		waiterDelegate.setWaiter(waiter);
+
+		waiterDelegate.service("XiaoGongJiang");
+
+	}
+}
---
+++ src/main/java/com/xgj/aop/spring/advisor/ControlFlowAdvisor/WaiterDelegate.java
@@ -0,0 +1,27 @@
+package com.xgj.aop.spring.advisor.ControlFlowAdvisor;
+
+public class WaiterDelegate {
+
+	private Waiter waiter;
+
+	public void setWaiter(Waiter waiter) {
+		this.waiter = waiter;
+	}
+
+	/**
+	 * 
+	 * 
+	 * @Title: service
+	 * 
+	 * @Description: waiter类中方法的调用，通过该方法发起
+	 * 
+	 * @param name
+	 * 
+	 * @return: void
+	 */
+	public void service(String name) {
+		waiter.greetTo(name);
+		waiter.serverTo(name);
+	}
+
+}
