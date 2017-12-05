43ddfd12ffc69fd5009a1ea4fb594c38a32005ff
Mr.Yang <815150141@qq.com>
2017-12-05 21:46:36 +0800

    Spring OXM-XStream使用别名

---
+++ src/main/java/com/xgj/oxm/xstream/quickDemo/aliasDemo/XStreamAliasDemo.java
@@ -0,0 +1,97 @@
+package com.xgj.oxm.xstream.quickDemo.aliasDemo;
+
+import java.text.ParseException;
+import java.util.Date;
+
+import com.thoughtworks.xstream.XStream;
+import com.xgj.oxm.xstream.quickDemo.domain.LoginLog;
+import com.xgj.oxm.xstream.quickDemo.domain.User;
+
+public class XStreamAliasDemo {
+
+	private static XStream xstream;
+	
+	static {
+		// 创建一个Xstream实例，使用默认的XPP解析器
+		xstream = new XStream();
+
+		// (1)设置类别名，修改默认的全限定名的名称
+		xstream.alias("user", User.class);
+		xstream.alias("loginLog", LoginLog.class);
+
+		// (2)设置类成员别名 <id>1</id> 改为<userId>1</userId>
+		xstream.aliasField("userId", User.class, "id");
+		
+		// (3)把LoginLog的userId属性视为xml属性，默认为xml的元素
+		xstream.aliasAttribute(LoginLog.class, "userId", "id");
+		xstream.useAttributeFor(LoginLog.class, "userId");
+		
+		// (4)去掉集合类型生成XML的父节点，即忽略xml中的<logs></logs>标记
+		xstream.addImplicitCollection(User.class, "logs");
+	}
+
+	/**
+	 * 
+	 * 
+	 * @Title: getUser
+	 * 
+	 * @Description: 初始化转换对象
+	 * 
+	 * @return
+	 * 
+	 * @return: User
+	 * @throws ParseException
+	 */
+	public static User getUser() throws ParseException {
+		LoginLog log = new LoginLog();
+		log.setIp("127.0.0.1");
+		log.setLoginLogId(99);
+		log.setUserId(1);
+		log.setLoginDate(new Date());
+
+		LoginLog log2 = new LoginLog();
+		log2.setIp("192.168.1.1");
+		log2.setLoginLogId(22);
+		log2.setUserId(2);
+		log2.setLoginDate(new Date());
+
+
+		User user = new User();
+		user.setId(1);
+		user.setUserName("Artisan");
+		user.setPassword("artisan");
+		user.setCredits(1000);
+		user.setLastVisit(new Date());
+
+		user.addLoginLog(log);
+		user.addLoginLog(log2);
+		return user;
+	}
+
+	/**
+	 * 
+	 * 
+	 * @Title: objectToXml
+	 * 
+	 * @Description: Java对象转换成XML
+	 * 
+	 * @throws Exception
+	 * 
+	 * @return: void
+	 */
+	public static void objectToXml() throws Exception {
+
+		// 获取转换的User对象实例
+		User user = getUser();
+		// 输出内容到控制台,查看一下
+		System.out.println(xstream.toXML(user));
+
+		System.out.println("objectToXml successfully");
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		objectToXml();
+	}
+
+}
